5b64bcd9aa70ab439da283bb30ca8b9b0a9a615f
Pranay Pawar <pranay93pawar@gmail.com>
2018-09-21 12:59:59 +0530

    commited initial ios project structure

---
+++ NetworkLayer/NetworkLayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NetworkLayer
+//
+//  Created by Mac-145-Pranay-Pawar on 21/09/18.
+//  Copyright © 2018 Mac-145-Pranay-Pawar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
